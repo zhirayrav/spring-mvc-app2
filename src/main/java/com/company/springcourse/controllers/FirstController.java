4a107e3f21d9f824b099e63e363f4ea00b726011
Zhirayr Avanesyan <zhirayr.avanesyan@mail.ru>
2022-09-13 21:28:20 +0300

    calculate in get method (use thymeleaf)

--- src/main/java/com/company/springcourse/controllers/FirstController.java
+++ src/main/java/com/company/springcourse/controllers/FirstController.java
@@ -1,18 +1,51 @@
 package com.company.springcourse.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 	@GetMapping("/hello")
-	public String helloPage() {
+	public String helloPage(@RequestParam(value ="name",required = false) String name,
+			@RequestParam(value = "surname",required = false) String surname,Model model) {
+		model.addAttribute("message","Hello " + name + " " + surname);
 		return "first/hello";
 	}
+	
+		
 	@GetMapping("/goodbye")
 	public String goodByePage() {
 		return "first/goodbye";
 	}
+	@GetMapping("/calculate")
+	public String calc(@RequestParam("a") int a, 
+			@RequestParam("b") int b,
+			@RequestParam(value = "action",required = false) String action,Model model) {
+		double result;
+		switch(action) {
+		case "addition" :
+			result = a+b;
+			break;
+		case "subtraction":
+			result = a-b;
+			break;
+		case "multiplication":
+			result = a*b;
+			break;
+		case "division":
+			result = a/(double)b;
+			break;
+		default: 
+			result = 0;
+			break;
+		}
+		model.addAttribute("result", result);
+		return "first/calculate";
+		}
 }
